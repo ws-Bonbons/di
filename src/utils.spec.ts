4adad9a5a45b0514ee6617749bd39b51d3ce8274
Big Mogician <sunaoran@youzan.com>
2019-02-14 16:08:02 +0800

    feat: more utils test

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -10,7 +10,11 @@ describe("@bonbons/di/src/utils.ts", () => {
 
   it("test utils features", () => {
     expect(typeof utils.Colors, "[utils.Colors] 's type ").to.equal("object");
-    expect(typeof utils.setColor, "[utils.Colors] 's type ").to.equal("function");
-    expect(typeof utils.isFunction, "[utils.Colors] 's type ").to.equal("function");
+
+    expect(typeof utils.setColor, "[utils.setColor] 's type ").to.equal("function");
+    expect(typeof utils.setColor("a", "b"), "[utils.setColor] 's return type ").to.equal("string");
+
+    expect(typeof utils.isFunction, "[utils.isFunction] 's type ").to.equal("function");
+    expect(typeof utils.isFunction({}), "[utils.isFunction] 's return type ").to.equal("boolean");
   });
 });
