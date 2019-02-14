f57173f3d829105cbc8b95005faf4e1e096d59a8
Big Mogician <sunaoran@youzan.com>
2019-02-14 16:13:41 +0800

    fix: better utils uts struct group

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -8,12 +8,16 @@ describe("@bonbons/di/src/utils.ts", () => {
     expect(Object.keys(utils).length, "[utils] exports count").to.equal(3);
   });
 
-  it("test utils features", () => {
+  it("test utils.Colors", () => {
     expect(typeof utils.Colors, "[utils.Colors] 's type ").to.equal("object");
+  });
 
+  it("test utils.setColor", () => {
     expect(typeof utils.setColor, "[utils.setColor] 's type ").to.equal("function");
     expect(typeof utils.setColor("a", "b"), "[utils.setColor] 's return type ").to.equal("string");
+  });
 
+  it("test utils.isFunction", () => {
     expect(typeof utils.isFunction, "[utils.isFunction] 's type ").to.equal("function");
     expect(typeof utils.isFunction({}), "[utils.isFunction] 's return type ").to.equal("boolean");
     expect(utils.isFunction(() => 123), "[utils.isFunction] check {function} ").to.equal(true);
